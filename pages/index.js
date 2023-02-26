76cf172f91745ed21653b8dcef10730fa8bb4e7a
charbriggs <charbriggs.dev@gmail.com>
2023-02-26 13:44:18 +0000

    imported navbar and photo to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
+import Link from "next/link";
 import Hero from "../components/home/hero";
 import Bio from "../components/home/bio";
-
+import Navbar from "../components/home/navbar";
+import Photo from "../components/home/photo";
 
 export default function Homepage() {
   return (
-    <div className="w-full py-10 bg-teal-100">
+    <div className="w-full py-10 bg-teal-100 pt-20">
+     <Navbar />
       <Hero
-        name="Charlotte"
+        name="Hi, I'm Charlotte"
       />
-  
+      <Photo />
       <Bio />
 
     </div>
