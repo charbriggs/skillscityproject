bba14b0edc88a411aed82ba8c08bd62b4446ad5e
charbriggs <charbriggs.dev@gmail.com>
2023-02-26 13:43:39 +0000

    added photo component.

---
+++ components/home/photo/index.js
@@ -0,0 +1,9 @@
+export default function Photo() {
+    return (
+      <div className="w-full max-w-6xl mx-auto w-3/12">
+        <div className="relative">
+         <img className="rounded-full w-96 h-90 shadow-xl dark:shadow-gray-400" src="/assets/headshot.jpg" />
+        </div>
+        </div>
+    )
+  }
