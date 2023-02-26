2666ff9bec26c75fed780aaacfdef5fc3e1246dc
charbriggs <charbriggs.dev@gmail.com>
2023-02-26 13:42:07 +0000

    adjusted padding and margins on bio.

--- components/home/bio/index.js
+++ components/home/bio/index.js
@@ -1,7 +1,7 @@
 export default function Bio() {
     return (
-      <div className="text-center text-gray-700 pt-6">
-        I am a software engineering student, a mum and I'm fluent in Brazilian Portuguese.
+      <div className="text-center text-gray-700 pt-6 mx-5 mt-3">
+        I am a software engineering student, with a passion for accessibility, a mum, a non-fiction book worm and I'm nearly fluent in Brazilian Portuguese. 
       </div>
     )
   }
