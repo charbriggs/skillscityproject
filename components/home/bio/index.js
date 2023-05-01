6e638ca1cb4b65954b1bebcf39068be410116d1f
charbriggs <charbriggs.dev@gmail.com>
2023-05-01 17:24:49 +0100

    Changed bio

--- components/home/bio/index.js
+++ components/home/bio/index.js
@@ -1,7 +1,15 @@
 export default function Bio() {
     return (
-      <div className="text-center text-gray-700 pt-6 mx-5 mt-3">
-        I am a software engineering student, with a passion for accessibility, a mum, a non-fiction book worm and I'm nearly fluent in Brazilian Portuguese. 
-      </div>
+      <div className="w-screen pl-10 ">
+      <div className="max-w-prose text-l text-center text-zinc-800pt-6 mx-5 mt-3">
+        Hello! I'm Charlotte Briggs Alves, a Full-Stack Software Developer from Manchester, UK.
+
+        Creating fully functional, beautifully designed and accessible web applications to fruition is what makes me tick.
+
+
+
+
+         
+      </div></div>
     )
   }
