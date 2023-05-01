e28153ee731a5433fb32d97ed784bc902835b43c
charbriggs <charbriggs.dev@gmail.com>
2023-05-01 17:33:25 +0100

    adjusted minor error with tailwind

--- components/home/bio/index.js
+++ components/home/bio/index.js
@@ -1,7 +1,7 @@
 export default function Bio() {
     return (
       <div className="w-screen pl-10 ">
-      <div className="max-w-prose text-l text-center text-zinc-800pt-6 mx-5 mt-3">
+      <div className="max-w-prose text-l text-center text-zinc-800 pt-6 mx-5 mt-3">
         Hello! I'm Charlotte Briggs Alves, a Full-Stack Software Developer from Manchester, UK.
 
         Creating fully functional, beautifully designed and accessible web applications to fruition is what makes me tick.
