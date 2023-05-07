6eca9b9e9c8ed6e00ca6ae16a5df4c547bbe3b7e
charbriggs <charbriggs.dev@gmail.com>
2023-05-07 14:24:29 +0100

    added hero section for homepage

---
+++ components/about/hero/index.js
@@ -0,0 +1,11 @@
+export default function Hero({ name }) {
+    return (
+      
+      <div className=" tracking-widest">
+        <div className="text-5xl text-akira text-center pt-10 pb-0 content-center">
+        <div className="font-bold text-violet-600 py-2">
+        {name}
+        </div>
+        </div></div>
+    )
+  }
