13744c1fd52f6e2e6c8e1aa3070dda82d7b898f8
charbriggs <charbriggs.dev@gmail.com>
2023-05-07 14:23:58 +0100

    added component for biography

---
+++ components/about/biography/index.js
@@ -0,0 +1,21 @@
+export default function About() {
+    return (
+      <div className="pb-0">
+        <div className="grid grid-cols-2 gap-0 ax-w-lg pb-14">
+         <img className="bg-contain bg-left rounded-lg drop-shadow-md max-w-md ml-48 mr-0" src="/assets/headshot.png" alt="woman with brown hair tied up in a bun with a long fringe draping her face, smiling and looking at the camera"/>
+         <div className=" max-w-prose text-l text-neutral-100 font-mono mr-80 mt-24 pr-0">
+        Hello! I'm Charlotte Briggs Alves, a Full-Stack Software Engineer from Manchester, UK.
+        <br />
+        <br />
+        Creating fully functional, beautifully designed and accessible web applications to fruition is what makes me tick.
+        <br />
+        <br />
+        Take a look around my projects and connect with me on LinkedIn.
+
+
+        </div>
+        </div>
+        </div>
+
+    )
+  }
