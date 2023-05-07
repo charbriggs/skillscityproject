648af1c9d278dded66fddc6b6a76eb68c208e68e
charbriggs <charbriggs.dev@gmail.com>
2023-05-07 14:26:25 +0100

    adjusted bio component with change to text

--- components/home/bio/index.js
+++ components/home/bio/index.js
@@ -2,7 +2,7 @@ export default function Bio() {
     return (
       <div className="bg-zinc-800">
       <div className=" max-w-prose text-l text-neutral-100 pb-8 pt-10 mx-5 mt-3 font-mono">
-        Hello! I'm Charlotte Briggs Alves, a Full-Stack Software Developer from Manchester, UK.
+        Hello! I'm Charlotte Briggs Alves, a Software Engineer from Manchester, UK.
         <br />
         <br />
         Creating fully functional, beautifully designed and accessible web applications to fruition is what makes me tick.
