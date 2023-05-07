658770e185075087367d9bcd9e0a28ebe65ea95c
charbriggs <charbriggs.dev@gmail.com>
2023-05-07 14:25:34 +0100

    added footer

---
+++ components/home/footer/index.js
@@ -0,0 +1,9 @@
+export default function Footer () {
+    return (
+        <div className="font-mono text-neutral-100 opacity-20 max-w-full text-center">
+            <footer>
+            Image by <a href="https://www.freepik.com/free-vector/gradient-black-background-with-wavy-lines_19836745.htm#from_view=detail_serie">Freepik</a>
+            </footer>
+        </div>
+    )
+}
