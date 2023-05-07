5fd62ba556e2f9e7b73beac8e6a1a05484200e30
charbriggs <charbriggs.dev@gmail.com>
2023-05-07 14:25:55 +0100

    moved skillsgrid component from home to aboutpage

---
+++ components/about/skillsgrid/index.js
@@ -0,0 +1,20 @@
+export default function Skillsgrid() {
+    return (
+        <div className="bg-zinc-600 bg-center rounded-xl border-2 border-violet-500 px-40 py-5 max-w-4xl mx-60 pb-12 opacity-75">
+             <div className="text-center pb-4 font-sansserif text-xl text-neutral-100 opacity-100 font-semibold"> Tech Stack </div>
+        <div className="grid grid-cols-4 gap-4 justify-normal content-center text-zinc-800">
+        <div className="bg-violet-500 text-neutral-100 hover:bg-zinc-800 hover:text-violet-400 rounded-md text-center font-mono"> HTML </div>
+        <div className="bg-violet-500 text-neutral-100 hover:bg-zinc-800 hover:text-violet-400 rounded-md text-center font-mono"> CSS </div>
+        <div className="bg-violet-500 text-neutral-100 hover:bg-zinc-800 hover:text-violet-400 rounded-md text-center font-mono"> Next.js</div>
+        <div className="bg-violet-500 text-neutral-100 hover:bg-zinc-800 hover:text-violet-400 rounded-md text-center font-mono"> Node.js</div>
+        <div className="bg-violet-500 text-neutral-100 hover:bg-zinc-800 hover:text-violet-400 rounded-md text-center font-mono"> Javascript</div>
+        <div className="bg-violet-500 text-neutral-100 hover:bg-zinc-800 hover:text-violet-400 rounded-md text-center font-mono"> Python</div>
+        <div className="bg-violet-500 text-neutral-100 hover:bg-zinc-800 hover:text-violet-400 rounded-md text-center font-mono"> PostgreSQL</div>
+        <div className="bg-violet-500 text-neutral-100 hover:bg-zinc-800 hover:text-violet-400 rounded-md text-center font-mono"> Tailwind CSS</div>
+        <div className="bg-violet-500 text-neutral-100 hover:bg-zinc-800 hover:text-violet-400 rounded-md text-center font-mono"> Bootstrap</div>
+
+        
+        </div>
+        </div>
+    )
+}
