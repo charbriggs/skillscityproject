a4d46a327a64940895d11ecc36498f31260928fe
charbriggs <charbriggs.dev@gmail.com>
2023-05-07 15:05:57 +0100

    adjusted function name

--- pages/projects.js
+++ pages/projects.js
@@ -1,13 +1,13 @@
 import Navbar from "../components/work/navbar";
 import Footer from "../components/work/footer";
-import Work from "../components/work/hero";
+import Headerone from "../components/work/hero";
 
 export default function Projectspage () {
     return (
         <div className="bg-background1 bg-cover font-akira scroll-smooth">
             <div className="">
             <Navbar />
-            <Work />
+            <Headerone />
             <Footer />
         </div>
         </div>
